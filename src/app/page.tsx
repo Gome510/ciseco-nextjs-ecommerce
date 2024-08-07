0ec9ea10dd3456a9221b4a910e8f2897f345e16d
Pablo Gomez-Echegaray <pgomezec@ucsc.edu>
2024-08-06 17:48:17 -0700

    blank slate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,66 +18,7 @@ import SectionGridFeatureItems from "@/components/SectionGridFeatureItems";
 import SectionMagazine5 from "@/app/blog/SectionMagazine5";
 
 function PageHome() {
-  return (
-    <div className="nc-PageHome relative overflow-hidden">
-      <SectionHero2 />
-
-      <div className="mt-24 lg:mt-32">
-        <DiscoverMoreSlider />
-      </div>
-
-      <div className="container relative space-y-24 my-24 lg:space-y-32 lg:my-32">
-        <SectionSliderProductCard
-          data={[
-            PRODUCTS[4],
-            SPORT_PRODUCTS[5],
-            PRODUCTS[7],
-            SPORT_PRODUCTS[1],
-            PRODUCTS[6],
-          ]}
-        />
-
-        <div className="py-24 lg:py-32 border-t border-b border-slate-200 dark:border-slate-700">
-          <SectionHowItWork />
-        </div>
-        <SectionPromo1 />
-
-        <div className="relative py-24 lg:py-32">
-          <BackgroundSection />
-          <SectionGridMoreExplore />
-        </div>
-
-        <SectionSliderProductCard
-          heading="Best Sellers"
-          subHeading="Best selling of the month"
-        />
-
-        <SectionPromo2 />
-
-        <SectionSliderLargeProduct cardStyle="style2" />
-
-        <SectionSliderCategories />
-
-        <SectionPromo3 />
-
-        <SectionGridFeatureItems />
-
-        <div className="relative py-24 lg:py-32">
-          <BackgroundSection />
-          <div>
-            <Heading rightDescText="From the Ciseco blog">
-              The latest news
-            </Heading>
-            <SectionMagazine5 />
-            <div className="flex mt-16 justify-center">
-              <ButtonSecondary>Show all blog articles</ButtonSecondary>
-            </div>
-          </div>
-        </div>
-        <SectionClientSay />
-      </div>
-    </div>
-  );
+  return <div className="nc-PageHome relative overflow-hidden"></div>;
 }
 
 export default PageHome;
